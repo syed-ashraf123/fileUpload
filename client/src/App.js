373ae71492fb9d246b77b17faf72d04a38c17986
Syed Ashraf <osamaashraf78@gmail.com>
2021-05-29 00:07:27 +0530

    Second Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import FormData from "form-data";
+import Axios from "axios";
+import { useState } from "react";
 function App() {
+  const [pic, setPic] = useState();
+
+  const upload = async (e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("video", pic);
+    Axios.post("https://132.145.174.217/emytNode/", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
+      <input
+        type="file"
+        id="pic"
+        onChange={(e) => {
+          setPic(e.target.files[0]);
+        }}
+      />
+      <button
+        onClick={(e) => {
+          upload(e);
+        }}
+      >
+        Send
+      </button>
     </div>
   );
 }
